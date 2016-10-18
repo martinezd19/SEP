fc678b63b13268fd5d68bf6abd9427628952150a
Dominic Martinez <martinezd19@trinityprep.org>
2016-10-18 15:14:18 -0400

    Add website

---
+++ website/indexc0d0.php
@@ -0,0 +1,72 @@
+<? xml version = "1.0" encoding = "utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title>AIO Inspections - Home</title>
+        <description><![CDATA[]]></description>
+        <link>
+        http://localhost:8012/joomla/index.php</link>
+        <lastBuildDate>Thu, 16 Jun 2016 21:36:11 +0000</lastBuildDate>
+        <generator>Joomla! - Open Source Content Management</generator>
+        <atom:link rel="self" type="application/rss+xml"
+                   href="http://localhost:8012/joomla/index.php?format=feed&amp;type=rss"/>
+        <language>en-gb</language>
+        <item>
+            <title>Pintex</title>
+            <link>
+            http://localhost:8012/joomla/index.php/welcome</link>
+            <guid isPermaLink="true">http://localhost:8012/joomla/index.php/welcome</guid>
+            <description><![CDATA[<p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                ]]>
+            </description>
+            <category>Home</category>
+            <pubDate>Fri, 02 May 2014 17:27:00 +0000</pubDate>
+        </item>
+        <item>
+            <title>Creativity</title>
+            <link>
+            http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/123-responsive-design</link>
+            <guid isPermaLink="true">
+                http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/123-responsive-design
+            </guid>
+            <description><![CDATA[<p><img src="http://localhost:8012/joomla/images/icon-1.png" alt=""/></p>
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                    labore.</p>
+                ]]>
+            </description>
+            <category>Home</category>
+            <pubDate>Tue, 21 Apr 2015 06:56:30 +0000</pubDate>
+        </item>
+        <item>
+            <title>Collaboration</title>
+            <link>
+            http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/124-clean-code</link>
+            <guid isPermaLink="true">
+                http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/124-clean-code
+            </guid>
+            <description><![CDATA[<p><img src="http://localhost:8012/joomla/images/icon-2.png" alt=""/></p>
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                    labore.</p>
+                ]]>
+            </description>
+            <category>Home</category>
+            <pubDate>Tue, 21 Apr 2015 06:56:30 +0000</pubDate>
+        </item>
+        <item>
+            <title>Connectivity</title>
+            <link>
+            http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/125-423-icons</link>
+            <guid isPermaLink="true">
+                http://localhost:8012/joomla/index.php/the-best-way-to-show-your-works-to-the-world/125-423-icons
+            </guid>
+            <description><![CDATA[<p><img src="http://localhost:8012/joomla/images/icon-3.png" alt=""/></p>
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                    labore.</p>
+                ]]>
+            </description>
+            <category>Home</category>
+            <pubDate>Tue, 21 Apr 2015 06:56:30 +0000</pubDate>
+        </item>
+    </channel>
+</rss>
